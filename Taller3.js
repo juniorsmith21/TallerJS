db7395ab444c5477d134b7f5ce6a871299c70fa3
juniorsmith21 <ordonezjun0@gmail.com>
2026-07-10 17:52:53 -0500

    Cambios

--- Taller3.js
+++ Taller3.js
@@ -2,36 +2,33 @@ class Vehiculo{
 marca;
 modelo;
 año;
-constructor(marca,modelo,año){
-this.marca=marca;
-this.modelo=modelo;
-this.año=año;
+constructor(marca, modelo, año) {
+this.marca = marca;
+this.modelo = modelo;
+this.año = año;
 }
-informacion(){
+informacion() {
     return `La marca del vehiculo es: ${this.marca}, su modelo es: ${this.modelo}, y su año es: ${this.año}`
 }
-
 
 }
 class Automovil extends Vehiculo{
     puertas;
-    constructor(marca,modelo,año,puertas){
-        super(marca,modelo,año)
-        this.puertas=puertas
+    constructor(marca, modelo, año, puertas) {
+        super(marca, modelo, año)
+        this.puertas = puertas
 
     }
-    informacion(){
+    informacion() {
         return `${super.informacion()}, Numero de puertas: ${this.puertas}`
     }
-    NoPuerta(puerta){
-        return this.puertas=puerta;
+    NoPuerta(puerta) {
+        return this.puertas = puerta;
 
     }
 
-
 }
-let automovilito=new Automovil("Hyundai","accent",2015,9)
+let automovilito = new Automovil("Hyundai", "accent", 2015, 9)
 console.log(automovilito.informacion());
 automovilito.NoPuerta(6);
 console.log(automovilito.informacion());
-
