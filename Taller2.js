74a8652ef775f433c872ee8714da673bb65c6a18
juniorsmith21 <ordonezjun0@gmail.com>
2026-07-10 17:51:10 -0500

    Update Taller2.js

--- Taller2.js
+++ Taller2.js
@@ -1,20 +1,20 @@
 class Rectangulo{
 ancho;
 alto;
- constructor(ancho,alto){
-    this.ancho=ancho;
-    this.alto=alto;
+ constructor(ancho, alto) {
+    this.ancho = ancho;
+    this.alto = alto;
  }
 
- calcularPerimetro(){
+ calcularPerimetro() {
     return `Perimnetro ${(2*(this.ancho + this.alto))}`
  }
-calcularArea(){
+calcularArea() {
     return `area: ${(this.ancho * this.alto)}cm^2`
 }
 }
 
-let rectangulito=new Rectangulo(5,9);
+let rectangulito = new Rectangulo(5, 9);
 console.log(rectangulito.calcularPerimetro());
 console.log(rectangulito.calcularArea());
 
