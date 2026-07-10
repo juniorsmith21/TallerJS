74bfe9baf0bdfe4ef5715b209d21514507e281ec
juniorsmith21 <ordonezjun0@gmail.com>
2026-07-10 17:59:39 -0500

    Cambios

--- Taller7.js
+++ Taller7.js
@@ -2,31 +2,30 @@ class Book{
 titulo;
 autor;
 añoPublicacion;
-constructor(titulo,autor,añoPublicacion){
-    this.titulo=titulo;
-    this.autor=autor;
-    this.añoPublicacion=añoPublicacion;
+constructor(titulo, autor, añoPublicacion) {
+    this.titulo = titulo;
+    this.autor = autor;
+    this.añoPublicacion = añoPublicacion;
 
 }
-informacion(){
+informacion() {
     return `Titulo: ${this.titulo}, autor: ${this.autor}, año de publicacion:${this.añoPublicacion}`
 }
 
 }
 class LibroE extends Book{
 precioLibro;
-constructor(titulo,autor,añoPublicacion,precioLibro) {
-    super(titulo,autor,añoPublicacion)
-    this.precioLibro=precioLibro;
-    
+constructor(titulo, autor, añoPublicacion, precioLibro) {
+    super(titulo, autor, añoPublicacion)
+    this.precioLibro = precioLibro;
+
 }
 
-informacion(){
+informacion() {
     return `${super.informacion()}, precio del libro: ${this.precioLibro}`
 }
 
-
 }
 
-let librito=new LibroE("Los magos ", "Rodri", 2011, 96500)
+let librito = new LibroE("Los magos ", "Rodri", 2011, 96500)
 console.log(librito.informacion());
